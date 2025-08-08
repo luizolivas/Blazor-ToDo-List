d8f04af6c44df325784f05f7b20e963bbf530d0e
luizolivas <luizolivas@gmail.com>
2025-08-08 10:53:00 -0300

    feat: cria model Tarefa

---
+++ BlazorToDoList/Data/Models/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorToDoList.Data.Models
+{
+    public class Tarefa
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
